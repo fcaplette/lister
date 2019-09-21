630b4a31709df5097973df437aff875b0b15de2f
Frederic Caplette <frederic@e-180.com>
2019-09-21 13:51:58 -0400

    chore: Remove unneeded dependency

--- src/shared/app/base/store/store.ts
+++ src/shared/app/base/store/store.ts
@@ -1,13 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducer/rootReducer";
 import thunk from "redux-thunk";
 
 export function configureStore(initialState = {}) {
-  return createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(thunk))
-  );
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
